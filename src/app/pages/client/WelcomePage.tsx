4efd6e77e3664e620fadc0f0f76ac4ef31755c6e
Wayne <wayne.tr.1711@gmail.com>
2025-08-01 10:21:30 +0700

    Update welcome page

--- src/app/pages/client/WelcomePage.tsx
+++ src/app/pages/client/WelcomePage.tsx
@@ -15,16 +15,15 @@ export function WelcomePage() {
         <PageHeroSection>
           <PageHero
             icon={<img width="70" height="70" src={CinnySVG} alt="Cinny Logo" />}
-            title="Welcome to Cinny"
+            title="Welcome to Pocket Wingman"
             subTitle={
               <span>
-                Yet another matrix client.{' '}
                 <a
                   href="https://github.com/cinnyapp/cinny/releases"
                   target="_blank"
                   rel="noreferrer noopener"
                 >
-                  v4.8.1
+                  v1.0.0
                 </a>
               </span>
             }
@@ -33,18 +32,7 @@ export function WelcomePage() {
               <Box grow="Yes" style={{ maxWidth: toRem(300) }} direction="Column" gap="300">
                 <Button
                   as="a"
-                  href="https://github.com/cinnyapp/cinny"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  before={<Icon size="200" src={Icons.Code} />}
-                >
-                  <Text as="span" size="B400" truncate>
-                    Source Code
-                  </Text>
-                </Button>
-                <Button
-                  as="a"
-                  href="https://cinny.in/#sponsor"
+                  href="https://lvbrd.xyz"
                   target="_blank"
                   rel="noreferrer noopener"
                   fill="Soft"
