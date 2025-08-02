e2b71960be1b3847fa6daf5d04fbb2b84069330d
Wayne <wayne.tr.1711@gmail.com>
2025-08-02 10:40:25 +0700

    Move to the right

--- src/app/features/room/Room.tsx
+++ src/app/features/room/Room.tsx
@@ -78,8 +78,8 @@ export function Room() {
                 variant="Primary"
                 style={{
                   position: 'fixed',
-                  bottom: '80px',
-                  left: '20px',
+                  top: '50px',
+                  right: '10px',
                   zIndex: 100,
                   borderRadius: '100%',
                   padding: '5px',
