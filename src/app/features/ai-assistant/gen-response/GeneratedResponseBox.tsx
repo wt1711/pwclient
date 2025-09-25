e7a844a6e92afef9bcbbf85967109d41e8b8676e
Wayne <wayne.tr.1711@gmail.com>
2025-09-25 13:15:28 +0700

    Update on background color

--- src/app/features/ai-assistant/gen-response/GeneratedResponseBox.tsx
+++ src/app/features/ai-assistant/gen-response/GeneratedResponseBox.tsx
@@ -19,6 +19,8 @@ export function GeneratedResponseBox() {
         display: 'flex',
         flexDirection: 'column',
         padding: '21px 19px 19px 19px',
+        background: 'rgba(255, 255, 255, 0.10)',
+        boxShadow: '0 40px 30px 0 rgba(0, 0, 0, 0.05), 0 1px 1px 0 rgba(255, 255, 255, 0.60) inset',
         backdropFilter: 'blur(50px)',
       }}
     >
